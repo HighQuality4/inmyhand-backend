b8f54db4e89e94770f334caa9278e6278ef1387c
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2025-04-15 16:54:22 +0900

    [feat] 엑스빌더 페이지 레이아웃 설계

---
+++ clx-src/app/layout/layout.js
@@ -0,0 +1,44 @@
+/************************************************
+ * layout.js
+ * Created at 2025. 4. 10. 오후 6:42:47.
+ *
+ * @author gyrud
+ ************************************************/
+
+let pathName = "";
+
+/*
+ * 루트 컨테이너에서 load 이벤트 발생 시 호출.
+ * 앱이 최초 구성된후 최초 랜더링 직후에 발생하는 이벤트 입니다.
+ */
+function onBodyLoad(e){
+	const subList = app.lookup("sms1");
+	subList.send();
+	
+	pathName = window.location.pathname;
+}
+
+/*
+ * 서브미션에서 submit-success 이벤트 발생 시 호출.
+ * 통신이 성공하면 발생합니다.
+ */
+function onSms1SubmitSuccess(e){
+
+	var embeddedRoutes = app.lookup("embeddedRoutes");
+	var voRow = embeddedRoutes.findFirstRow("path == '" + pathName + "'");
+	
+	if (voRow != null) {
+		const vsAppId = voRow.getValue("app");
+		const vcEmb = app.lookup("ea1");
+		
+		cpr.core.App.load(vsAppId, function(loadedApp){ 
+			if(loadedApp){
+				vcEmb.app = loadedApp; 
+			} 
+		});
+		
+		app.getContainer().redraw();	
+	}
+	
+   
+}
