5259fc9fbcb6514c5ab9f5a0d43a4ca9eab48243
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-18 18:16:27 +0900

    [chore] 로그인서비스 반환타입 수정

--- src/main/java/com/inmyhand/refrigerator/member/service/LoginService.java
+++ src/main/java/com/inmyhand/refrigerator/member/service/LoginService.java
@@ -1,4 +1,11 @@
 package com.inmyhand.refrigerator.member.service;
 
+import com.inmyhand.refrigerator.member.domain.dto.LoginRequestDTO;
+import com.inmyhand.refrigerator.member.domain.dto.LoginResponseDTO;
+import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
+
+import java.util.Optional;
+
 public interface LoginService {
+    public LoginResponseDTO login(LoginRequestDTO loginRequestDTO);
 }
