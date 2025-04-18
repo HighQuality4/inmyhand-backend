9722c8dab735edab9789db4561e61bcf57768b3c
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-18 18:13:52 +0900

    [chore] member컨트롤러 수정

--- src/main/java/com/inmyhand/refrigerator/member/controller/MemeberController.java
+++ src/main/java/com/inmyhand/refrigerator/member/controller/MemeberController.java
@@ -1,4 +1,24 @@
 package com.inmyhand.refrigerator.member.controller;
 
+import com.inmyhand.refrigerator.member.domain.dto.LoginRequestDTO;
+import com.inmyhand.refrigerator.member.domain.dto.LoginResponseDTO;
+import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
+import com.inmyhand.refrigerator.member.service.LoginServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
 public class MemeberController {
+
+    private final LoginServiceImpl loginService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> ctlLogin(@RequestBody LoginRequestDTO loginRequestDTO) {
+        LoginResponseDTO member = loginService.login(loginRequestDTO);
+        return ResponseEntity.ok().body(member);
+    }
 }
