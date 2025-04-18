db6abb18af823030039cc64838a0aadc4e26b5e4
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-18 18:11:10 +0900

    [chore] LoginRequestDTO 생성

---
+++ src/main/java/com/inmyhand/refrigerator/member/domain/dto/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package com.inmyhand.refrigerator.member.domain.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequestDTO {
+    private String email;       // 아이디 대체 가능
+    private String password;    // 평문 전달 (암호화는 내부 처리)
+}
+
