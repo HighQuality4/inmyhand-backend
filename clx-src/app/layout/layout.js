fb528a74775b9c6301bfecef33fdfa11dff3c7cc
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2025-04-18 18:07:07 +0900

    [fix] 페이지 라우터 id값도 처리될 수 있도록 로직 수정

--- clx-src/app/layout/layout.js
+++ clx-src/app/layout/layout.js
@@ -5,22 +5,16 @@
  * @author gyrud
  ************************************************/
 
-const embeddedRoutesTest = [
-  { path: "/recipe", loadApp: "app/recipe/recipe_main" },
-  { path: /^\/recipe\/(\d+)$/, loadApp: "app/recipe/recipe_detail" },
-];
-
+const embeddedRoutesModule = cpr.core.Module.require("data/embeddedRoutes");
+const embeddedRoutes = embeddedRoutesModule.embeddedRoutes;
 /*
  * 루트 컨테이너에서 load 이벤트 발생 시 호출.
  * 앱이 최초 구성된후 최초 랜더링 직후에 발생하는 이벤트 입니다.
  */
 function onBodyLoad(e){
-	const subList = app.lookup("sms1");
-	subList.send();
-	
 	const pathName = window.location.pathname;
 	
-	const match = embeddedRoutesTest.find(route => {
+	const match = embeddedRoutes.find(route => {
 	  if (typeof route.path === "string") {
 	    return route.path === pathName;
 	  } else if (route.path instanceof RegExp) {
@@ -42,28 +36,3 @@ function onBodyLoad(e){
 		app.getContainer().redraw();	
 	}
 }
-
-/*
- * 서브미션에서 submit-success 이벤트 발생 시 호출.
- * 통신이 성공하면 발생합니다.
- */
-//function onSms1SubmitSuccess(e){
-//
-//	var embeddedRoutes = app.lookup("embeddedRoutes");
-//	var voRow = embeddedRoutes.findFirstRow("path == '" + pathName + "'");
-//	
-//	if (voRow != null) {
-//		const vsAppId = voRow.getValue("app");
-//		const vcEmb = app.lookup("ea1");
-//		
-//		cpr.core.App.load(vsAppId, function(loadedApp){ 
-//			if(loadedApp){
-//				vcEmb.app = loadedApp; 
-//			} 
-//		});
-//		
-//		app.getContainer().redraw();	
-//	}
-//	
-//   
-//}
