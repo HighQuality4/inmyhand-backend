8357e503e212410584bdb19f1d9097f5fca90f85
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-19 17:22:02 +0900

    [chore] 리프레시토큰 리포지토리 메서드 추가

--- src/main/java/com/inmyhand/refrigerator/member/repository/RefreshTokenRepository.java
+++ src/main/java/com/inmyhand/refrigerator/member/repository/RefreshTokenRepository.java
@@ -1,5 +1,12 @@
 package com.inmyhand.refrigerator.member.repository;
 
-public interface RefreshTokenRepository {
+import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
+import com.inmyhand.refrigerator.member.domain.entity.RefreshTokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
+
+    RefreshTokenEntity findByMemberEntity(MemberEntity member);
 }
