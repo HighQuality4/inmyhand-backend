4073db877523eb215640c6ca3f3cadb92c6781a3
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-21 17:55:55 +0900

    [chore] 리프레시토큰 조건 나눠서 갱신 및 삽입 조건 분리

--- src/main/java/com/inmyhand/refrigerator/security/Oauth2UserDetailService.java
+++ src/main/java/com/inmyhand/refrigerator/security/Oauth2UserDetailService.java
@@ -7,8 +7,11 @@ import com.inmyhand.refrigerator.member.domain.entity.RefreshTokenEntity;
 import com.inmyhand.refrigerator.member.domain.enums.MemberRole;
 import com.inmyhand.refrigerator.member.repository.LoginRepository;
 import com.inmyhand.refrigerator.member.repository.MemberRepository;
+import com.inmyhand.refrigerator.member.repository.RefreshTokenRepository;
 import com.inmyhand.refrigerator.security.jwt.JwtTokenUtil;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -27,12 +30,15 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
+@Transactional
 @RequiredArgsConstructor
 public class Oauth2UserDetailService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final JwtTokenUtil jwtTokenUtil;
     private final LoginRepository loginRepository;
     private final MemberRepository memberRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
@@ -42,33 +48,41 @@ public class Oauth2UserDetailService implements OAuth2UserService<OAuth2UserRequ
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
         String email = null;
+        String name = null;
         String nickname = null;
         String accessToken = null;
         String provider = registrationId;
 
-        if (registrationId.equals("naver")) {
+        if (registrationId.equals("naver")) { //네이버
             Map<String, Object> response = (Map<String, Object>) attributes.get("response"); //네이버는 사용자 정보가 response 안에 있음.
             email = (String) response.get("email");
             nickname = (String) response.get("nickname");
-        } else if (registrationId.equals("google")) {
+            name = (String) response.get("name");
+        } else if (registrationId.equals("google")) { //구글
             email = (String) attributes.get("email");
             nickname = (String) attributes.get("name");
-        } else if (registrationId.equals("kakao")) {
+            name = (String) attributes.get("name");
+        } else if (registrationId.equals("kakao")) { //카카오
             Map<String, Object> response = (Map<String, Object>) attributes.get("kakao_account");
             Map<String, Object> profile = (Map<String, Object>) response.get("profile");
             email = (String) response.get("email");
             nickname = (String) profile.get("nickname");
+            name = (String) profile.get("name");
         }
 
         MemberEntity member = loginRepository.findByEmail(email).orElse(null);
+        RefreshTokenEntity existedToken = refreshTokenRepository.findByMemberEntity(member).orElse(null);
+        System.out.println("member: " + member);
 
         if (member == null) {
             member = MemberEntity.builder()
                     .email(email)
+                    .memberName(name)
                     .nickname(nickname)
                     .providerId(provider)
                     .regdate(new Date())
                     .build();
+            memberRepository.save(member);
 
             accessToken = jwtTokenUtil.generateAccessToken(member);
             MemberRoleEntity role = new MemberRoleEntity();
@@ -80,14 +94,20 @@ public class Oauth2UserDetailService implements OAuth2UserService<OAuth2UserRequ
             refreshTokenEntity.setTokenValue(jwtTokenUtil.generateRefreshToken(member));
             refreshTokenEntity.setExpiredAt(Timestamp.valueOf(LocalDateTime.now().plusDays(14))); // 예: 2주 유효
             refreshTokenEntity.setMemberEntity(member);
+            refreshTokenRepository.save(refreshTokenEntity);
+            refreshTokenRepository.flush();
 
         } else {
+            if (existedToken == null) {
+                existedToken = new RefreshTokenEntity();
+                existedToken.setMemberEntity(member);
+            }
             //리프레시 토큰 값만 갱신
             String refreshToken = jwtTokenUtil.generateRefreshToken(member);
-            RefreshTokenEntity refreshTokenEntity = new RefreshTokenEntity();
-            refreshTokenEntity.setTokenValue(refreshToken);
-            refreshTokenEntity.setExpiredAt(Timestamp.valueOf(LocalDateTime.now().plusDays(7)));
-            refreshTokenEntity.setMemberEntity(member);
+            existedToken.setTokenValue(refreshToken);
+            existedToken.setExpiredAt(Timestamp.valueOf(LocalDateTime.now().plusDays(14)));
+            refreshTokenRepository.save(existedToken);
+            refreshTokenRepository.flush();
         }
 
         List<GrantedAuthority> authorities = member.getMemberRoleList().stream()
