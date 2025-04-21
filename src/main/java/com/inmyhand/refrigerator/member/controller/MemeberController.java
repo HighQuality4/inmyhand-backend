44f501ee5cef43c9cd0e4bda3185de61b98ba2df
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-21 10:57:26 +0900

    [chore] 컨트롤러에 DataRequest 형식 추가

--- src/main/java/com/inmyhand/refrigerator/member/controller/MemeberController.java
+++ src/main/java/com/inmyhand/refrigerator/member/controller/MemeberController.java
@@ -1,9 +1,11 @@
 package com.inmyhand.refrigerator.member.controller;
 
+import com.cleopatra.protocol.data.DataRequest;
 import com.inmyhand.refrigerator.member.domain.dto.LoginRequestDTO;
 import com.inmyhand.refrigerator.member.domain.dto.LoginResponseDTO;
 import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
 import com.inmyhand.refrigerator.member.service.LoginServiceImpl;
+import com.inmyhand.refrigerator.util.ConverterClassUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +20,12 @@ public class MemeberController {
     private final LoginServiceImpl loginService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> ctlLogin(@RequestBody LoginRequestDTO loginRequestDTO, HttpServletResponse response) {
-        LoginResponseDTO member = loginService.login(loginRequestDTO, response);
-        return ResponseEntity.ok().body(member);
+    public ResponseEntity<LoginResponseDTO> ctlLogin(DataRequest loginRequestDTO, HttpServletResponse response) {
+
+        System.out.println("받은 값: " + loginRequestDTO.toString());
+        LoginResponseDTO loginResponseDTO = ConverterClassUtil.getSingleClass(loginRequestDTO, "dmLogin", LoginResponseDTO.class);
+        System.out.println(loginResponseDTO.toString());
+        //LoginResponseDTO member = loginService.login(loginRequestDTO, response);
+        return ResponseEntity.ok(loginResponseDTO);
     }
 }
