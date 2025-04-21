6abe15b98fb36c8cdf23a9230ff91cc2cb5014a6
yeongbee <handfaste@naver.com>
2025-04-21 16:29:41 +0900

    [fix] CleopatraUIController 최적화 수정

--- src/main/java/com/inmyhand/refrigerator/web/CleopatraUIController.java
+++ src/main/java/com/inmyhand/refrigerator/web/CleopatraUIController.java
@@ -1,38 +1,28 @@
 package com.inmyhand.refrigerator.web;
 
-
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.View;
-
 import com.cleopatra.spring.UIView;
 import com.cleopatra.ui.PageGenerator;
-
 import jakarta.annotation.PostConstruct;
-
-
 
 @Controller
 public class CleopatraUIController {
 
-	public CleopatraUIController() {
-	}
+    @PostConstruct
+    private void initPageGenerator() {
+        PageGenerator.getInstance().setURLSuffix(".clx");
+    }
 
-	@PostConstruct
-	private void initPageGenerator(){
-		PageGenerator instance = PageGenerator.getInstance();
-		instance.setURLSuffix(".clx");
-	}
+    @RequestMapping(value = {"/**/*.clx", "/*.clx"})
+    public View handleClxRequests() {
+        return new UIView();
+    }
 
-	@RequestMapping("/**/*.clx")
-	public View index() {
-		return new UIView();
-	}
-
-	@RequestMapping("/*.clx")
-	public View index2() {
-		return new UIView();
-	}
-
+    @GetMapping(value = {"/{path:[^\\.]*}","/**/{path:[^\\.]*}"})
+    public String forwardToLayout() {
+        return "/app/layout/layout";
+    }
 }
