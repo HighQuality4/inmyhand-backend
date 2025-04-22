06079bc8a75266ab4eb3c1bd6f2395161dfc67d4
JungHaCho <09ange33@gmail.com>
2025-04-22 23:52:34 +0900

    [fix] 삭제된 파일 복구하기

--- clx-src/data/embeddedRoutes.module.js
+++ clx-src/data/embeddedRoutes.module.js
@@ -2,7 +2,8 @@ const embeddedRoutes = [
   { path: "/recipe", loadApp: "app/recipe/recipe_main" }, // 정적 경로
   { path: /^\/recipe\/(\d+)$/, loadApp: "app/recipe/recipe_detail" }, // id 값이 있는 경우
   { path: "/recipe/search", loadApp: "app/recipe/recipe_search" }, // 쿼리 파라미터가 있는 경우
-  { path: "/auth/login", loadApp: "app/login/login" }
+  { path: "/auth/login", loadApp: "app/login/login" },
+  { path: "/fridge", loadApp: "app/fridge/fridge_main" }
 ];
 
 exports.embeddedRoutes = embeddedRoutes;
