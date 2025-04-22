23292355af8e264d92d9669e9396a0e442c624a1
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-22 21:57:14 +0900

    [chore] 멤버 컨트롤러에 테스트 주소 추가

--- src/main/java/com/inmyhand/refrigerator/member/controller/MemeberController.java
+++ src/main/java/com/inmyhand/refrigerator/member/controller/MemeberController.java
@@ -5,6 +5,8 @@ import com.inmyhand.refrigerator.member.domain.dto.LoginRequestDTO;
 import com.inmyhand.refrigerator.member.domain.dto.LoginResponseDTO;
 import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
 import com.inmyhand.refrigerator.member.service.LoginServiceImpl;
+import com.inmyhand.refrigerator.member.service.MemberServiceImpl;
+import com.inmyhand.refrigerator.member.service.RegisterServiceImpl;
 import com.inmyhand.refrigerator.util.ConverterClassUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,14 @@ import java.util.Optional;
 public class MemeberController {
 
     private final LoginServiceImpl loginService;
+    private final RegisterServiceImpl registerService;
+
+    @PostMapping("/login/test")
+    public ResponseEntity<LoginResponseDTO> ctlLoginTest(@RequestBody LoginRequestDTO loginDto, HttpServletResponse response) {
+        LoginResponseDTO responseDTO = loginService.login(loginDto, response);
+        return ResponseEntity.ok(responseDTO);
+    }
+
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> ctlLogin(DataRequest loginRequestDTO, HttpServletResponse response) {
@@ -27,5 +37,11 @@ public class MemeberController {
         System.out.println(loginResponseDTO.toString());
         //LoginResponseDTO member = loginService.login(loginRequestDTO, response);
         return ResponseEntity.ok(loginResponseDTO);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> ctlRegister(@RequestBody MemberEntity memberEntity, HttpServletResponse response) {
+        registerService.registerMember(memberEntity);
+        return ResponseEntity.ok("가입이 완료되었습니다!");
     }
 }
