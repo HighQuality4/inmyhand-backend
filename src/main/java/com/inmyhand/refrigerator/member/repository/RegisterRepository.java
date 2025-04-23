5b6b5e15818f1eb9554858339b0a1a815da0e88d
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-24 02:21:39 +0900

    [refactor] 기존 레지스터 관련 부분 멤버 서비스, 리포지토리 등에 통합

--- src/main/java/com/inmyhand/refrigerator/member/repository/RegisterRepository.java
+++
@@ -1,8 +0,0 @@
-package com.inmyhand.refrigerator.member.repository;
-
-import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegisterRepository extends JpaRepository<MemberEntity, Integer> {
-
-}
