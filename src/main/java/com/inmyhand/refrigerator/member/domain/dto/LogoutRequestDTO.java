b89761c56c6e251fecacc86168c5b74337b0a32e
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-24 02:20:06 +0900

    [refactor] 로그인 로그아웃 관련 컨트롤러, 로그아웃 DTO 추가 및 정리

---
+++ src/main/java/com/inmyhand/refrigerator/member/domain/dto/LogoutRequestDTO.java
@@ -0,0 +1,15 @@
+package com.inmyhand.refrigerator.member.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogoutRequestDTO {
+    private Long id;
+    private String refreshToken;
+}
