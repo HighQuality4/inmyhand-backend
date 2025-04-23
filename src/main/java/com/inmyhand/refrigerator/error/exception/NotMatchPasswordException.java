8b70d65a600576e5c499ae4544d2d3cce0f6a7a6
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-24 02:19:20 +0900

    [feat] NOTMATCHPASSWORDEXCEPTION 예외 추가

---
+++ src/main/java/com/inmyhand/refrigerator/error/exception/NotMatchPasswordException.java
@@ -0,0 +1,5 @@
+package com.inmyhand.refrigerator.error.exception;
+
+public class NotMatchPasswordException extends RuntimeException {
+    public NotMatchPasswordException(String message) {super(message);}
+}
