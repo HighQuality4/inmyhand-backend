3082a6b592010ae3f976b10ec5a1cd403f5fd083
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-24 02:36:23 +0900

    [refactor] 로컬 가입 방식 코드 구조 개선 및 status 포함

--- src/main/java/com/inmyhand/refrigerator/member/service/MemberService.java
+++ src/main/java/com/inmyhand/refrigerator/member/service/MemberService.java
@@ -1,4 +1,8 @@
 package com.inmyhand.refrigerator.member.service;
 
+import com.inmyhand.refrigerator.member.domain.dto.MemberDTO;
+import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
+
 public interface MemberService {
+    MemberEntity registerLocal(MemberDTO memberDTO);
 }
--- src/main/java/com/inmyhand/refrigerator/member/service/MemberServiceImpl.java
+++ src/main/java/com/inmyhand/refrigerator/member/service/MemberServiceImpl.java
@@ -1,7 +1,11 @@
 package com.inmyhand.refrigerator.member.service;
 
+import com.inmyhand.refrigerator.member.domain.dto.MemberDTO;
+import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
+import com.inmyhand.refrigerator.member.domain.enums.MemberStatus;
 import com.inmyhand.refrigerator.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +13,23 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public MemberEntity registerLocal(MemberDTO memberDTO) {
+
+        MemberEntity memberEntity = MemberEntity.builder()
+                .memberName(memberDTO.getMemberName())
+                .nickname(memberDTO.getNickname())
+                .password(passwordEncoder.encode(memberDTO.getPassword()))
+                .email(memberDTO.getEmail())
+                .phoneNum(memberDTO.getPhoneNum())
+                .providerId("LOCAL")
+                .status(MemberStatus.active)
+                .build();
+
+        return memberRepository.save(memberEntity);
+    }
 
 //    public String getMemberProfileImage(Long memberId) {
 //        return memberRepository.findFileUrlsByMemberId(memberId);
