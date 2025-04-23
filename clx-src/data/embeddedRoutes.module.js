53df3163f29999f94e8028354f93891fc3f7ee1c
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-24 02:09:32 +0900

    [chore] 주소 매핑 수정

--- clx-src/data/embeddedRoutes.module.js
+++ clx-src/data/embeddedRoutes.module.js
@@ -2,7 +2,8 @@ const embeddedRoutes = [
   { path: "/recipe", loadApp: "app/recipe/recipe_main" }, // 정적 경로
   { path: /^\/recipe\/(\d+)$/, loadApp: "app/recipe/recipe_detail" }, // id 값이 있는 경우
   { path: "/recipe/search", loadApp: "app/recipe/recipe_search" }, // 쿼리 파라미터가 있는 경우
-  { path: "/auth/login", loadApp: "app/login/login" }
+  { path: "/users/login", loadApp: "app/login/login" },
+  { path: "/users/mypage", loadApp: "app/mypage/mypage"}
 ];
 
 exports.embeddedRoutes = embeddedRoutes;
