8779f54a5402723a3dc956796e46ed3d6c7a9589
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-24 02:34:55 +0900

    [chore] import문 정리 및 엑스빌더 환경에 맞춰서 로그아웃 방식 변경

--- src/main/java/com/inmyhand/refrigerator/member/service/LogoutServiceImpl.java
+++ src/main/java/com/inmyhand/refrigerator/member/service/LogoutServiceImpl.java
@@ -1,35 +1,24 @@
 package com.inmyhand.refrigerator.member.service;
 
-import com.google.api.gax.rpc.ApiException;
-import com.inmyhand.refrigerator.security.jwt.JwtTokenUtil;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import com.inmyhand.refrigerator.common.redis.RedisKeyManager;
+import com.inmyhand.refrigerator.common.redis.RedisUtil;
+import com.inmyhand.refrigerator.member.repository.RefreshTokenRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class LogoutServiceImpl implements LogoutService {
 
-    private final JwtTokenUtil jwtTokenUtil;
-
-    public void logout(String accessToken, String refreshToken) {
-        //  1. AccessToken 검증
-        if(!jwtTokenUtil.validateToken(accessToken)) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다!");
-        }
-
-        // 2. AccessToken에서 사용자 식별(userId)
-        String userIdStr = jwtTokenUtil.getUsernameFromToken(accessToken);
-        Long userId = Long.parseLong(userIdStr);
-
-        // 3. DB에서 해당 사용자의 refreshToken 삭제
-
-        // 4. AccessToken을 Redis 블랙리스트에 등록
-
+    private final RedisUtil redisUtil;
+    private final RedisKeyManager redisKeyManager;
+    private final RefreshTokenRepository refreshTokenRepository;
+    
+    @Override
+    @Transactional
+    public void logout(Long userId) {
+        refreshTokenRepository.deleteByMemberEntityId(userId);
+        redisUtil.delete(redisKeyManager.getLoginKey(userId));
     }
 }
