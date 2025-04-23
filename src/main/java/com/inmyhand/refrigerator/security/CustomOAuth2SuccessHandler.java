35caed7af2c1215647ef0e7fdb0a5b08a5a7f1ba
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-24 08:59:52 +0900

    [chore] Success 핸들러에서 쿠키 설정 및 리다이렉트 경로 수정

--- src/main/java/com/inmyhand/refrigerator/security/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/inmyhand/refrigerator/security/CustomOAuth2SuccessHandler.java
@@ -23,16 +23,25 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
 
         CustomOAuth2User user = (CustomOAuth2User) authentication.getPrincipal();
         String accessToken = user.getAccessToken();
-        System.out.println("accessToken: " + accessToken);
         // HTTPOnly 쿠키 설정
         ResponseCookie cookie = ResponseCookie.from("access_token", accessToken)
                 .httpOnly(true)
                 .secure(false) // production이면 true로 유지
-                .sameSite("Lax") // 또는 "Strict" / "None" (CORS 환경 따라)
+                .sameSite("Strict") // 또는 "Strict" / "None" (CORS 환경 따라)
                 .path("/")
                 .maxAge(Duration.ofMinutes(30))
                 .build();
         response.setHeader("Set-Cookie", cookie.toString());
+        
+        
+        ResponseCookie userIdCookie = ResponseCookie.from("userId", String.valueOf(user.getMember().getId()))
+                .httpOnly(false) //JS에서 접근 가능
+                .sameSite("Lax")
+                .path("/")
+                .maxAge(Duration.ofDays(7))
+                .build();
+
+        response.addHeader("Set-Cookie", userIdCookie.toString());
 
         //request.getSession().removeAttribute("SPRING_SECURITY_SAVED_REQUEST");
 
@@ -50,6 +59,6 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
         }
 
         // 프론트엔드 페이지로 리다이렉트
-        response.sendRedirect("http://localhost:7079/auth/login"); // 실제 프론트 페이지 주소
+        response.sendRedirect("http://localhost:7079/users/mypage"); // 실제 프론트 페이지 주소
     }
 }
