d5d95e5dd110673ef0b2aa1d0ae88e9cf2974f3d
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-27 02:04:25 +0900

    [chore] 마이페이지 관련 로직 임시 구현

--- clx-src/app/mypage/mypage.js
+++ clx-src/app/mypage/mypage.js
@@ -6,6 +6,15 @@
  ************************************************/
 //로그아웃 처리하는 메서드가 인자로 액세스 토큰 값을 받는데 js에선 그걸 못하니까 유저 아이디 값을 로컬 스토리지에 저장.
 
+
+function getCookieValue(name) {
+   const cookie = document.cookie
+        .split('; ')
+        .find(row => row.indexOf(name + '=') === 0);
+    
+    return cookie ? cookie.split('=')[1] : null;
+}
+
 /*
  * "로그아웃" 버튼에서 click 이벤트 발생 시 호출.
  * 사용자가 컨트롤을 클릭할 때 발생하는 이벤트.
@@ -14,7 +23,13 @@ function onButtonClick(e) {
 	
 	var btn = e.control;
 	
-	const userId = localStorage.getItem("userId");
+	const userId = localStorage.getItem("userId") || getCookieValue("userId");
+	
+	if (!userId) {
+        alert("로그인 정보가 없습니다. (401 Unauthorized)");
+        return;
+    }
+	
     var dm = app.lookup("dmLogout");
     dm.setValue("userId", userId);
     app.lookup("smsLogout").send(); // 서버 전송
