225f78a2373371739e6f4da62a4aae444c142e86
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-27 03:13:38 +0900

    [chore] import 수정

--- src/main/java/com/inmyhand/refrigerator/member/service/MemberService.java
+++ src/main/java/com/inmyhand/refrigerator/member/service/MemberService.java
@@ -1,7 +1,6 @@
 package com.inmyhand.refrigerator.member.service;
 
 import com.inmyhand.refrigerator.member.domain.dto.MemberDTO;
-import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
 
 public interface MemberService {
     boolean registerLocal(MemberDTO memberDTO);
