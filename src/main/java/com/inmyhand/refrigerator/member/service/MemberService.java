1e5d5a5d116d21c3d6f7e8652bf0e9b4c000759b
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-27 02:08:22 +0900

    [chore] 로컬 회원가입 서비스 리턴형 변경

--- src/main/java/com/inmyhand/refrigerator/member/service/MemberService.java
+++ src/main/java/com/inmyhand/refrigerator/member/service/MemberService.java
@@ -4,5 +4,5 @@ import com.inmyhand.refrigerator.member.domain.dto.MemberDTO;
 import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
 
 public interface MemberService {
-    MemberEntity registerLocal(MemberDTO memberDTO);
+    boolean registerLocal(MemberDTO memberDTO);
 }
