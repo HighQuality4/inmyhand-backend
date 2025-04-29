299c5b69e0efd0bff86950fba5e3a0068c382cf5
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-30 00:52:09 +0900

    [chore] 로그인 페이지 경로 수정

--- clx-src/data/embeddedRoutes.module.js
+++ clx-src/data/embeddedRoutes.module.js
@@ -3,12 +3,12 @@ const embeddedRoutes = [
   { path: "/recipe", loadApp: "app/recipe/recipe_main" },
   { path: /^\/recipe\/(\d+)$/, loadApp: "app/recipe/recipe_detail" },
   { path: "/recipe/search", loadApp: "app/recipe/recipe_search" },
-  { path: "/users/login", loadApp: "app/login/login" },
+  { path: "/auth/login", loadApp: "app/login/login" },
   { path: "/users/mypage", loadApp: "app/mypage/mypage"},
   { path: "/fridge", loadApp: "app/fridge/fridge_main" },
   { path: "/recipe/form", loadApp: "app/recipe/recipe_form" }, // 레시피 등록
   { path: /^\/recipe\/form\/(\d+)$/, loadApp: "app/recipe/recipe_form" }, // 레시피 수정
-  { path: "/users/register", loadApp: "app/login/register"},
+  { path: "/auth/register", loadApp: "app/login/register"},
   { path: "/fridge", loadApp: "app/fridge/fridge_main" },
   { path: "/fridge/auto", loadApp: "app/fridge/fridge_auto" }
 ];
