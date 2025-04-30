e3d7175cafa35ca3cfb728901c482707a77b3c33
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-04-30 13:21:45 +0900

    [chore] 로그인 리포지토리 수정

--- src/main/java/com/inmyhand/refrigerator/member/repository/LoginRepository.java
+++ src/main/java/com/inmyhand/refrigerator/member/repository/LoginRepository.java
@@ -1,6 +1,7 @@
 package com.inmyhand.refrigerator.member.repository;
 
 import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -38,5 +39,6 @@ import java.util.Optional;
 
 @Repository
 public interface LoginRepository extends JpaRepository<MemberEntity, Long> {
+    @EntityGraph(attributePaths = "memberRoleList")
     Optional<MemberEntity> findByEmail(String email);
 }
