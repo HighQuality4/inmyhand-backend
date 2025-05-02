0561a92ddc4e3a199555ea029f017aa884f442b5
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-05-02 16:31:15 +0900

    [chore] 멤버 관련 냉장고 정보 제공 위한 코드 추가

--- src/main/java/com/inmyhand/refrigerator/member/repository/MemberRepository.java
+++ src/main/java/com/inmyhand/refrigerator/member/repository/MemberRepository.java
@@ -2,13 +2,16 @@ package com.inmyhand.refrigerator.member.repository;
 
 
 import com.inmyhand.refrigerator.admin.dto.MemberEntityDto;
+import com.inmyhand.refrigerator.member.domain.dto.MyFoodInfoDTO;
 import com.inmyhand.refrigerator.member.domain.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long>, CustomQueryMemberRepository {
@@ -22,5 +25,15 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long>, Cus
             "FROM MemberEntity m " +
             "ORDER BY m.id ASC ")
     List<MemberEntityDto> findAllMemberDto();
+
+    @Query(value = """
+    SELECT fd.food_name AS foodName, TO_CHAR(fd.end_date, 'YYYY-MM-DD') AS expdate
+    FROM fridge_member fm
+    JOIN fridge_food fd ON fm.fridge_id = fd.fridge_id
+    WHERE fm.member_id = :memberId
+    ORDER BY fd.end_date ASC
+    LIMIT 5
+""", nativeQuery = true)
+    public List<MyFoodInfoDTO> findMyRefreInfo(@Param("memberId") Long memberId);
 
 }
