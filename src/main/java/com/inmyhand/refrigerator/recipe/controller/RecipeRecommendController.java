39e2da2eb85df074762301e507adbbdf87166997
yeongbee <handfaste@naver.com>
2025-05-04 13:18:39 +0900

    [Feat] 레시피 사용자 재료기반 맞춤 추천 기능

--- src/main/java/com/inmyhand/refrigerator/recipe/controller/RecipeRecommendController.java
+++ src/main/java/com/inmyhand/refrigerator/recipe/controller/RecipeRecommendController.java
@@ -1,9 +1,12 @@
 package com.inmyhand.refrigerator.recipe.controller;
 
 import com.inmyhand.refrigerator.recipe.domain.dto.RecipeRecommendationDTO;
+import com.inmyhand.refrigerator.recipe.domain.dto.RecipeSummaryDTO;
 import com.inmyhand.refrigerator.recipe.service.RecipeRecommendationServiceImpl;
+import com.inmyhand.refrigerator.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +20,25 @@ public class RecipeRecommendController {
 
     private final RecipeRecommendationServiceImpl recipeRecommendationService;
 
-    @GetMapping("/test/{memberId}")
-    public ResponseEntity<Map<String, Object>> getRecommendations(@PathVariable("memberId") Long memberId) {
-        List<RecipeRecommendationDTO> recommendations = recipeRecommendationService.recommendRecipes(memberId);
+    // 유사도 나옴, 1에 가까울수록 정확함
+    @GetMapping("/test")
+    public ResponseEntity<Map<String, Object>> getRecommendations(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        Long userId = customUserDetails.getUserId();
+        // TODO 유저 아이디가 없다면 처리
+//        List<RecipeRecommendationDTO> recommendations = recipeRecommendationService.recommendRecipes(userId);
+        List<RecipeRecommendationDTO> recommendations = recipeRecommendationService.recommendRecipes(2L);
         return ResponseEntity.ok(Map.of("recommend", recommendations));
     }
 
-    @GetMapping("/test2/{memberId}")
-    public ResponseEntity<Map<String, Object>> getRecommendations2(@PathVariable("memberId") Long memberId) {
-        return ResponseEntity.ok(Map.of("recommend", recipeRecommendationService.recommendRecipes2(memberId)));
+
+    // 유사도 안나옴, Summary DTO 가 나옴
+    @GetMapping("/test2")
+    public ResponseEntity<Map<String, Object>> getRecommendations2(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        Long userId = customUserDetails.getUserId();
+        // TODO 유저 아이디가 없다면 처리
+//        List<RecipeSummaryDTO> recipeSummaryDTOS = recipeRecommendationService.recommendRecipes2(userId);
+        List<RecipeSummaryDTO> recipeSummaryDTOS = recipeRecommendationService.recommendRecipes2(2L);
+        return ResponseEntity.ok(Map.of("recommend", recipeSummaryDTOS));
     }
 
 }
