78075a3405ab6173ade9c217763824f61be1ad97
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-05-04 13:52:30 +0900

    [chore] 마이페이지 환영 문구 수정

--- clx-src/app/mypage/mypage.js
+++ clx-src/app/mypage/mypage.js
@@ -45,5 +45,24 @@ function onSmsLogoutReceive(e){
 	
 }
 
+/*
+ * 루트 컨테이너에서 load 이벤트 발생 시 호출.
+ * 앱이 최초 구성된후 최초 랜더링 직후에 발생하는 이벤트 입니다.
+ */
+function onBodyLoad(e){
+	var name = app.lookup("name");
+	var smsName = app.lookup("smsName");
+	
+	 smsName.addEventListener("submit-success", function(e) {
+        var nicknameValue = name.getValue("nickname");
 
+        var nickname = app.lookup("nickname");
 
+        nickname.value = nicknameValue;
+        console.log("✅ 닉네임:", nicknameValue);
+    });
+	
+	smsName.send();
+	
+	
+}
