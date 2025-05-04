4d9eca8d61b766bb22b71ec9bfa0202f9c84574d
sk-choi <80041090+sk-choi@users.noreply.github.com>
2025-05-05 03:37:57 +0900

    [chore] 마이페이지 history 적용

--- clx-src/app/mypage/mypage.js
+++ clx-src/app/mypage/mypage.js
@@ -73,5 +73,14 @@ function onBodyLoad(e){
  */
 function onButtonClick2(e){
 	var button = e.control;
-	window.location.href = window.location.origin + "/users/payment"
+	history.pushState({}, '', `/users/payment`);
+}
+
+/*
+ * "내 정보 변경하기" 버튼에서 click 이벤트 발생 시 호출.
+ * 사용자가 컨트롤을 클릭할 때 발생하는 이벤트.
+ */
+function onButtonClick3(e){
+	var button = e.control;
+	history.pushState({}, '', `/users/myinfo`);
 }
