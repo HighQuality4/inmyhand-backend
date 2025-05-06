4a75cead12998f5d8a6498382686829ba8a810f7
HyoKyoungLee <43572462+HyoKyoungLee@users.noreply.github.com>
2025-05-06 21:19:34 +0900

    [fix] 최종 udc.js 업로드

--- src/main/resources/view/cpr-lib/udc.js
+++ src/main/resources/view/cpr-lib/udc.js
@@ -2307,7 +2307,7 @@
 						    //const group = udc.lookup("foodinfogroup");
 						    refUdc.setNum(i+1);
 						    refUdc.setFoodName(foodName);
-						    const expText = Number(expdate) < 0 ? "유통기한이 지났어요!" : expdate + "일";
+						    const expText = Number(expdate) < 0 ? "유통기한이 지났어요!" : expdate + "일 남았어요!";
 							refUdc.setExpdate(expText);
 				//		    group.lookup("num").value = (i + 1) + ".";
 				//		    group.lookup("foodname").value = foodName;
